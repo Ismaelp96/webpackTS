b6c6d6f90e5204d51fd0292add430d04f3d61408
Ismael Oliveira <ismael6991@live.com>
2023-02-16 18:43:44 -0300

    Copiando os Arquivos da Pasta Pública

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const CopyPlugin = require('copy-webpack-plugin')
 
 module.exports = {
   mode: 'production', // devolopment
@@ -7,6 +8,11 @@ module.exports = {
     filename: 'app.min.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: 'public' }],
+    }),
+  ],
   resolve: {
     extensions: ['.ts', '.js'],
   },
