128285f05b6d1ed5a22c9d613c6f978a74516fb0
Ismael Oliveira <ismael6991@live.com>
2023-02-16 18:29:26 -0300

    Configurando o Build no Arquivo 'webpack.config.js'

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import Livro from './model/book'
+import $ from 'jquery'
+const livro = new Livro('Dom Quixote', 108.8, 0.1)
+// console.log(livro.precoComDesconto())
+
+$('body').append(`<h1>${livro.nome}</h1>`)
+$('body').append(`<h2>Preço: R$${livro.precoComDesconto()}</h2>`)
